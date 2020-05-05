34f36a2b82c31c96ba95cec2b2ecbae2013ba72d
roach <giraftw2002@gmail.com>
2020-05-05 21:42:42 +0800

    fix ios build by deleting page tag in html

--- README.md
+++ README.md
@@ -9,10 +9,18 @@ tns build android
 1. apt-get install mosquitto on the linux box
 2. sudo service mosquitto restart
 3. turn to use node mqtt to publish msg by adding mqtt codes in server.js
-4. test on browser: 
+4. test on browser:
     upload codes to nodemcu
      http://ajoan.com/moveCar?devId=xxxx&payload=50,10,0,0
+5. to keep track of logs - mosquitto -v
 
 
-   
+ios:
+    remove <pag> tag, otherwise bulid error arises.
+    disable https in iphone settings
+    in iphone, settings>general>profiles & device mgmt to establish trust for the developer.
+    tns platform add ios
+    tns prepare ios
+    tns deploy ios when iphone is plugged in.
+    getting signing profile and certificate in xcode.
 
--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -43,7 +43,7 @@ export class HomeComponent implements OnInit, OnDestroy {
 
     moveCar(s: IrobotState): any {
         // if no return here, it will fire an error at runtime. don't know why?
-        // return this.mqtt.callArest(s.autoPilot === true ? cmdEnum.AUTO : s.direction, s.speed.toString()) 
+        // return this.mqtt.callArest(s.autoPilot === true ? cmdEnum.AUTO : s.direction, s.speed.toString())
         return this.mqtt.publish('moveCar', this.devId, s);
     }
     // when tap on button, there a down, many move... an up events.
@@ -85,7 +85,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     robotState$: Observable<IrobotState> = this.robotCommands$
         .pipe(
             startWith(this.initialRobotState),
-            // ** touch event 'move' keeps being fired as long as not releasing.           
+            // ** touch event 'move' keeps being fired as long as not releasing.
             scan((state: IrobotState, command) => {
                 return ({ ...state, ...command });
             }),
@@ -94,12 +94,12 @@ export class HomeComponent implements OnInit, OnDestroy {
             shareReplay(1)
         );
 
-    navMode$ = this.robotState$.pipe(selectDistinctState('autoPilot'));    
-    direction$ = this.robotState$.pipe(        
+    navMode$ = this.robotState$.pipe(selectDistinctState('autoPilot'));
+    direction$ = this.robotState$.pipe(
         selectDistinctState('direction'),
         // filter out any direction emissions if autopilot is on
-        withLatestFrom(this.navMode$), 
-        filter(([dir, nav])=>nav===0))      
+        withLatestFrom(this.navMode$),
+        filter(([dir, nav])=>nav===0))
 
     // ** discard emitted values that take < 1s coz inputvalue keeps firing when sliding on slider.
     speed$: Observable<any> = this.robotState$.pipe(selectDistinctState('speed')).pipe(debounceTime(1000));
@@ -109,18 +109,18 @@ export class HomeComponent implements OnInit, OnDestroy {
         .pipe(
             // withLatestFrom takes 2 obs$, in this case we ignore 1st one(direction$), and take state$ only
             withLatestFrom(this.robotState$, (_, s) => s),
-          
-            // replace tap w/ exhaustMap so any coming direction event will be ignore if moveCar isn't completed. 
+
+            // replace tap w/ exhaustMap so any coming direction event will be ignore if moveCar isn't completed.
             // tap( console.log('s.direction') ),
             // debounce is to prevent sneding stop right after direction cmd if slightly touch
-            debounceTime(1200),
+            debounceTime(100),
             // switchmap is only for switching obs$ to another obs$. whereas in here s isn't obs$
             map((s: IrobotState) => this.moveCar(s))
         )
 
     constructor(private mqtt: MqttProvider) {
         // this.robotState$.subscribe(console.log);
-        // this.direction$.subscribe(console.log);       
+        // this.direction$.subscribe(console.log);
         // ** this console.log shows everything!
         this.navSubscription = this.navigation$.subscribe((res: number) => {
             // see if init http request successfully.
@@ -131,14 +131,14 @@ export class HomeComponent implements OnInit, OnDestroy {
                     console.log("Dialog closed!");
                 });
                 // alert(res.message+res.id);
-            } 
+            }
             */
         })
     }
 
     ngOnInit(): void {
-        // this.robotCommands$.subscribe(console.log);  
-        // start to receive commands  
+        // this.robotCommands$.subscribe(console.log);
+        // start to receive commands
         this.mqtt.sub('devId').subscribe(data => {
             this.devId = data;
             // console.log('devId: ', this.devId);
